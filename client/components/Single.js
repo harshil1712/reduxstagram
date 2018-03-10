e8fb01fbd867c5e26e4792f0d488a0d8effea358
Harshil Agrawal <ghagrawal17@gmail.com>
2018-03-11 00:38:32 +0530

    added comment form

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Photo from './photo';
+import Comments from './Comments'
 
 class Single extends Component{
     render(){
         const i = this.props.posts.findIndex((post)=> post.code === this.props.params.postID);
         const post = this.props.posts[i];
+        const postComments = this.props.comments[this.props.params.postID] || [];
         // console.log(i);
         return(
             <div className='single-photo'>
                 <Photo i={i} post={post} {...this.props} />
+                <Comments postComments={postComments} />
             </div>
         )
     }
