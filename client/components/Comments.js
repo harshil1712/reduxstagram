241c47eb92289564174e53044eb5e45383c8fe02
Harshil Agrawal <ghagrawal17@gmail.com>
2018-03-12 01:49:28 +0530

    comment add and remove

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -7,18 +7,27 @@ class Comments extends Component{
                 <p>
                     <strong>{comment.user}</strong>
                     {comment.text}
-                    <button className='remove-comment'>&times;</button>
+                    <button className='remove-comment' onClick={this.props.removeComment.bind(null,this.props.params.postID,i)}>&times;</button>
                 </p>
             </div>
         )
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        const postId = this.props.params.postID;
+        const author = this.refs.author.value;
+        const comment = this.refs.comment.value;
+        console.log(postId,author,comment);
+        this.props.addComment(postId,author,comment);
+        this.refs.commentForm.reset();
     }
     render(){
         return(
             <div className='comment'>
                 {this.props.postComments.map(this.renderComment)}
-                <form ref='commentForm' className='comment-form'>
-                    <input type='text' ref='author' placeholder='author' />
-                    <input type='text' ref='comment' placeholder='comment' />
+                <form ref='commentForm' className='comment-form' onSubmit={this.handleSubmit}>
+                    <input type='text' ref="author" placeholder='author' />
+                    <input type='text' ref="comment" placeholder='comment' />
                     <input type='submit' hidden />
                 </form>
             </div>
